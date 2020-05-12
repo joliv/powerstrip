7e376d26b515cb7c0e1a988915d354fcfa44eb38
Jack Ward <joliv@users.noreply.github.com>
2020-05-11 18:32:24 -0700

    Clear out for the rewrite

--- compress.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _DELTACODE_HPP_
-#define _DELTACODE_HPP_
-
-#include <cstdint>
-
-int64_t deltacode(uint16_t* inbuf, size_t insize, char* outbuf);
-
-#endif // _DELTACODE_HPP_
--- decompress.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _DELTADECODE_HPP_
-#define _DELTADECODE_HPP_
-
-#include <cstdint>
-#include <cstddef>
-
-int64_t deltadecode(int16_t* inbuf, size_t insize, uint16_t* outbuf);
-
-#endif // _DELTADECODE_HPP_
